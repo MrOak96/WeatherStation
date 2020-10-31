93c0212b25b3d977c0e4a3ac9c7e64808ff5757a
T3CHNOG4B <gabdeschesnes@gmail.com>
2020-10-31 15:47:39 -0400

    T03 GetTempCommand_ExecuteIfNullService_ShouldThrowNullException : Done

--- WeatherApp/ViewModels/TemperatureViewModel.cs
+++ WeatherApp/ViewModels/TemperatureViewModel.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Media.Animation;
 using WeatherApp.Commands;
+using WeatherApp.Models;
+using WeatherApp.Services;
 
 namespace WeatherApp.ViewModels
 {
     public class TemperatureViewModel : BaseViewModel
     {
-        /// TODO : Ajoutez le code nécessaire pour réussir les tests et répondre aux requis du projet
+        /// TODO : Ajoutez le code nécessaire pour réussir les tests et répondre aux requis du projet 
+        public ITemperatureService TemperatureService;
+        public DelegateCommand<String> GetTempCommand { get; set; }
+        public TemperatureModel CurrentTemp { get; set; }
+
+        public TemperatureViewModel()
+        {
+            GetTempCommand = new DelegateCommand<string>(GetTemperature);
+        }
+
+        private void GetTemperature(string obj)
+        {
+            if (TemperatureService == null)
+                throw new NullReferenceException();
+        }
         public static double CelsiusInFahrenheit(double c)
         {
             return Math.Round(c * 9 / 5 + 32, 1);
